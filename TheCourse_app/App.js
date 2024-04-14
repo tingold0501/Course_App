4df35142b1af105cee62b240e3427bbac31ec551
Phạm Huỳnh Tín <65479150+tingold0501@users.noreply.github.com>
2024-04-14 10:10:43 +0700

    ....

---
+++ TheCourse_app/App.js
@@ -0,0 +1,21 @@
+import { StatusBar } from 'expo-status-bar';
+import { StyleSheet, Text, View } from 'react-native';
+import {NavigationContainer} from '@react-navigation/native';
+import {createNativeStackNavigator} from '@react-navigation/native-stack';
+import OnboardOne from './src/screens/OnboardOne';
+const Stack = createNativeStackNavigator();
+export default function App() {
+  return (
+    <NavigationContainer>
+    <Stack.Navigator>
+      <Stack.Screen
+        name="Home"
+        component={OnboardOne}
+        options={{headerShown: false}}
+      />
+    </Stack.Navigator>
+  </NavigationContainer>
+  );
+}
+
+
---
+++ TheCourse_app/src/screens/OnboardOne.js
@@ -0,0 +1,23 @@
+import React from 'react'
+import { ImageBackground, Text, View } from 'react-native'
+import Carousel from 'react-native-snap-carousel';
+export default function OnboardOne() {
+    return (
+        <View style={{ flex: 1 }}>
+            <View style={{ flex: 1 }}>
+                <ImageBackground
+                    source={require('../../assets/images/onboard1.png')}
+                    style={{ width: '100%', height: '100%' }}
+                />
+            </View>
+            <View style={{ flex: 1 }}>
+                <View style={{ flex: 2, justifyContent: 'center', alignItems: 'center' }}>
+                    <Text style={{ fontSize: 30, fontWeight: 'bold', color: '#F9AD2B' }}>Find a field that you like</Text>
+                    <Text style={{ fontSize: 15, textAlign: 'center', color: 'gray' }}>There are many fields that you can find here, and you can learn all of them</Text>
+                </View>
+                <View style={{ flex: 1 }}>
+                </View>
+            </View>
+        </View>
+    )
+}
