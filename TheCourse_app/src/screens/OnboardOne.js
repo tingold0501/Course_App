60a0acd10dd3c8ab29b634d8b032d4364a624301
Phạm Huỳnh Tín <65479150+tingold0501@users.noreply.github.com>
2024-04-18 13:49:08 +0700

    ....

--- TheCourse_app/src/screens/OnboardOne.js
+++ TheCourse_app/src/screens/OnboardOne.js
@@ -1,22 +1,36 @@
-import React from 'react'
-import { ImageBackground, Text, View } from 'react-native'
-export default function OnboardOne() {
-    return (
-        <View style={{ flex: 1 }}>
-            <View style={{ flex: 1 }}>
-                <ImageBackground
-                    source={require('../../assets/images/onboard1.png')}
-                    style={{ width: '100%', height: '100%' }}
-                />
-            </View>
-            <View style={{ flex: 1 }}>
-                <View style={{ flex: 2, justifyContent: 'center', alignItems: 'center' }}>
-                    <Text style={{ fontSize: 30, fontWeight: 'bold', color: '#F9AD2B' }}>Find a field that you like</Text>
-                    <Text style={{ fontSize: 15, textAlign: 'center', color: 'gray' }}>There are many fields that you can find here, and you can learn all of them</Text>
-                </View>
-                <View style={{ flex: 1 }}>
-                </View>
-            </View>
-        </View>
-    )
-}
+import React, { useEffect, useState } from 'react';
+import { View, Text, ActivityIndicator } from 'react-native';
+import axios from 'axios';
+
+const OnboardOne = () => {
+  const [data, setData] = useState(null);
+  const [loading, setLoading] = useState(true);
+
+  
+  useEffect(() => {
+    fetch("http://localhost/api/getAllDataUser")
+      .then((res) => res.json())
+      .then((res) => {
+        console.log(res);
+      })
+      .catch((error) => console.error('Error fetching user data:', error));
+  }, []);
+
+  return (
+    <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
+      {loading ? (
+        <ActivityIndicator size="large" color="blue" />
+      ) : (
+        <>
+          {data ? (
+            <Text>{JSON.stringify(data)}</Text> // Hiển thị dữ liệu trả về
+          ) : (
+            <Text>No data available</Text>
+          )}
+        </>
+      )}
+    </View>
+  );
+};
+
+export default OnboardOne;
